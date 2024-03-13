fe5fb2c10b49066f2325631fa711111c1fbf5380
Nam Nguyen <nguyennam18401@gmail.com>
2024-03-13 17:02:58 +0700

    fix button form

--- next/src/components/AppTitle.tsx
+++ next/src/components/AppTitle.tsx
@@ -4,7 +4,7 @@ import BannerBadge from "./BannerBadge";
 
 const AppTitle = () => {
   return (
-    <div id="title" className="relative flex flex-col items-center">
+    <div id="title" className="relative mt-16 flex flex-col items-center">
       <div className="flex flex-row items-start">
         <span
           className="text-4xl font-bold text-slate-12 xs:text-5xl sm:text-7xl"
@@ -15,18 +15,14 @@ const AppTitle = () => {
           Viettrip Agent
         </span>
       </div>
-      <div className="mt-3 text-center font-mono text-[0.7em] font-bold text-white">
+      <div className="mt-5 text-center font-mono text-[0.7em] font-bold text-white">
         <div>
-          <BannerBadge
-            className="md:hidden"
-          >
-            Plan your next adventure
-          </BannerBadge>
+          <BannerBadge className="md:hidden">Plan your next adventure</BannerBadge>
         </div>
-        <div
-          className="hidden md:flex"
-        >
-          <BannerBadge>Turn your next trip into a hassle-free experience with Viettrip Agent.</BannerBadge>
+        <div className="hidden md:flex">
+          <BannerBadge>
+            Turn your next trip into a hassle-free experience with Viettrip Agent.
+          </BannerBadge>
         </div>
       </div>
     </div>
--- next/src/components/index/landing.tsx
+++ next/src/components/index/landing.tsx
@@ -11,6 +11,7 @@ import Button from "../Button";
 import ExampleAgents from "../console/ExampleAgents";
 import { ToolsDialog } from "../dialog/ToolsDialog";
 import Input from "../Input";
+import { MdAddToPhotos } from "react-icons/md";
 
 type LandingProps = {
   messages: Message[];
@@ -29,8 +30,36 @@ const Landing = (props: LandingProps) => {
   const { t } = useTranslation("indexPage");
   const agent = useAgentStore.use.agent();
 
+  const [formData, setFormData] = useState({
+    location: "",
+    start_Location: "",
+    start_Date: "",
+    end_Date: "",
+    number_Of_People: "",
+    budget: "",
+    age: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formDataString = JSON.stringify(formData);
+    console.log("Form data:", formDataString);
+    if (props.setAgentRun) {
+      props.setAgentRun(formData.location, formDataString);
+    }
+  };
+
   return (
     <>
+      <ToolsDialog show={showToolsDialog} setOpen={setShowToolsDialog} />
       <motion.div
         initial={{ opacity: 0, y: -50 }}
         animate={{ opacity: 1, y: 0 }}
@@ -48,29 +77,149 @@ const Landing = (props: LandingProps) => {
       >
         <ExampleAgents setAgentRun={props.setAgentRun} setShowSignIn={props.setShowSignInDialog} />
       </motion.div>
-      
+
       <motion.div
         initial={{ opacity: 0, y: 50 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.75, delay: 0.5, type: "easeInOut" }}
         className="z-10 flex w-full flex-col gap-6"
       >
-        <Input
-          inputRef={props.goalInputRef}
-          left={
-            <>
-              <FaStar />
-              <span className="ml-2">{`${t("LABEL_AGENT_GOAL")}`}</span>
-            </>
-          }
-          disabled={agent != null}
-          value={props.goalInput}
-          onChange={(e) => props.setGoalInput(e.target.value)}
-          onKeyDown={props.handleKeyPress}
-          placeholder={`${t("PLACEHOLDER_AGENT_GOAL")}`}
-          type="textarea"
-        />
+        <div>
+          <form className="mx-auto" onSubmit={handleSubmit}>
+            <div className="mb-5">
+              <label
+                htmlFor="location"
+                className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Where do you want to go?
+              </label>
+              <input
+                type="text"
+                id="location"
+                name="location"
+                value={formData.location}
+                onChange={handleInputChange}
+                className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+              />
+            </div>
 
+            <div className="mb-5">
+              <label
+                htmlFor="start_Location"
+                className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Where you start?
+              </label>
+              <input
+                type="text"
+                id="start_Location"
+                name="start_Location"
+                value={formData.start_Location}
+                onChange={handleInputChange}
+                className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+              />
+            </div>
+
+            <div className="mb-5">
+              <label
+                htmlFor="start_Date"
+                className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Start date
+              </label>
+              <input
+                type="date"
+                id="start_Date"
+                name="start_Date"
+                value={formData.start_Date}
+                onChange={handleInputChange}
+                className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+              />
+            </div>
+
+            <div className="mb-5">
+              <label
+                htmlFor="end_Date"
+                className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+              >
+                End Date
+              </label>
+              <input
+                type="date"
+                id="end_Date"
+                name="end_Date"
+                value={formData.end_Date}
+                onChange={handleInputChange}
+                className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+              />
+            </div>
+
+            <div className="mb-5">
+              <label
+                htmlFor="number_Of_People"
+                className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+              >
+                How many people are going?
+              </label>
+              <input
+                type="number"
+                min={1}
+                id="number_Of_People"
+                name="number_Of_People"
+                value={formData.number_Of_People}
+                onChange={handleInputChange}
+                className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+              />
+            </div>
+
+            <div className="mb-5">
+              <label
+                htmlFor="age"
+                className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+              >
+                * Average age of trip participants? (Optional)
+              </label>
+              <select
+                id="age"
+                name="age"
+                className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                value={formData.age}
+                onChange={handleInputChange}
+              >
+                <option value="">None</option>
+                <option value="under 30 years">under 30 years</option>
+                <option value="30 to 40 years">30 to 40 years</option>
+                <option value="40 to 50 years">40 to 50 years</option>
+                <option value="50 to 60 years">50 to 60 years</option>
+                <option value="65 to older">65 to older</option>
+              </select>
+            </div>
+
+            <div className="mb-5">
+              <label
+                htmlFor="budget"
+                className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+              >
+                * How much do you plan to spend on this trip? (Optional)
+              </label>
+              <select
+                id="budget"
+                name="budget"
+                className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                value={formData.budget}
+                onChange={handleInputChange}
+              >
+                <option value="">None</option>
+                <option value="Less than 500 USD">Less than 500 USD</option>
+                <option value="Between 500 and 1000 USD">Between 500 and 1000 USD</option>
+                <option value="Between 1000 and 2000 USD">Between 1000 and 2000 USD</option>
+                <option value="Between 2000 and 5000 USD">Between 2000 and 5000 USD</option>
+                <option value="Between 5000 and 10000 USD">Between 5000 and 10000 USD</option>
+                <option value="More than 10000 USD">More than 10000 USD</option>
+              </select>
+            </div>
+          </form>
+        </div>
         <div className="flex w-full flex-row items-center justify-center gap-3">
           <Button
             ping
@@ -80,12 +229,10 @@ const Landing = (props: LandingProps) => {
             <FaCog />
           </Button>
           <Button
-            onClick={
-              props.handlePlay
-            }
+            onClick={handleSubmit}
             className="my-5 border-0 bg-gradient-to-t from-[#02FCF1] to-[#A02BFE] subpixel-antialiased saturate-[75%] hover:saturate-100"
           >
-            <FaPlay />
+            <MdAddToPhotos /> 
           </Button>
         </div>
       </motion.div>
